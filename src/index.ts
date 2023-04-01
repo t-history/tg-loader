e4ca406e469c45270b1df0059ec7702117c75575
Moiseev Ilya <mois.ilya@gmail.com>
2023-04-01 17:13:06 +0400

    Created ChatList class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import config from './config'
 import client from './client'
 import ChatHistory from './ChatHistory'
+import ChatList from './ChatList'
 
 async function main (): Promise<void> {
   await client.login()
   const testChatId = config.testChatId ?? 0
 
   const chatHistoryInstance = new ChatHistory(client, testChatId)
-  await chatHistoryInstance.getChatHistory(10, 0)
+  await chatHistoryInstance.fetchChatHistory(10, 0)
+
+  const chatListInstance = new ChatList(client)
+  await chatListInstance.fetchChats()
 }
 
 main().catch((err: Error) => {
