4731e53ac27d420ebccf7d147e9e84ca92280a74
Moiseev Ilya <mois.ilya@gmail.com>
2023-05-01 01:30:29 +0400

    Added support for any platforms

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,11 @@
 import path from 'path'
+import { platform } from 'os'
 import { Client } from 'tdl'
 import { TDLib } from 'tdl-tdlib-addon'
 import config from './config'
 
-const tdl = new TDLib(path.join(__dirname, 'bin/libtdjson.dylib'))
+const ext = platform() === 'darwin' ? '.dylib' : platform() === 'win32' ? '.dll' : '.so'
+const tdl = new TDLib(path.join(__dirname, `bin/libtdjson${ext}`))
 
 const client = new Client(tdl, {
   apiId: config.apiId,
