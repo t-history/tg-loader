44808844347badc16611602c67dff602abe5e78b
Moiseev Ilya <mois.ilya@gmail.com>
2023-05-04 06:52:27 +0400

    Added linux support

--- src/client.ts
+++ src/client.ts
@@ -3,10 +3,13 @@ import { platform } from 'os'
 import { Client } from 'tdl'
 import { TDLib } from 'tdl-tdlib-addon'
 import config from './config'
+import { getTdjson } from 'prebuilt-tdlib'
 
 const ext = platform() === 'darwin' ? '.dylib' : platform() === 'win32' ? '.dll' : '.so'
-const connection = path.join(__dirname, `../bin/libtdjson${ext}`)
-const tdl = platform() === 'darwin' ? new TDLib(connection) : new TDLib()
+const connection = process.arch === 'arm64'
+  ? path.join(__dirname, `../bin/libtdjson${ext}`)
+  : getTdjson()
+const tdl = new TDLib(connection)
 
 const client = new Client(tdl, {
   apiId: config.apiId,
